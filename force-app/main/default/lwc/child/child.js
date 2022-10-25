37f58244e5255b781f5aadc8de031b3bc3157387
CTS\546122 <sudhirRamachal.Tiwari@cognizant.com>
2022-10-25 12:54:43 +0530

    Child To Parent Communication Uing Custom Event

--- force-app/main/default/lwc/child/child.js
+++ force-app/main/default/lwc/child/child.js
@@ -2,4 +2,9 @@ import { LightningElement,api } from 'lwc';
 
 export default class Child extends LightningElement {
     @api messageInChild;
+
+    sendMessageToParent(event){
+        const sendMesageFromChild = new CustomEvent('childmessage',{detail:'Hi I am from Child'});
+        this.dispatchEvent(sendMesageFromChild);
+    }
 }
--- force-app/main/default/lwc/parent/parent.js
+++ force-app/main/default/lwc/parent/parent.js
@@ -2,7 +2,12 @@ import { LightningElement,track } from 'lwc';
 
 export default class Parent extends LightningElement {
     @track parentMessage;
+    @track messageFromChild;
     sendDataToChild(event){
         this.parentMessage="Data from Parent!!!";
+    }
+
+    handleChildMessage(event){
+        this.messageFromChild = event.detail;
     }
 }
